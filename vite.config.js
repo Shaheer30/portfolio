4fbc9f1907e659ad481f8449f3c54eab83c4d924
shaheer-007 <shaheer.siddiqui@bmglobalinc.com>
2025-10-18 05:36:30 +0500

    vite.config updated

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
   },
+  assetsInclude: ['**/*.xml', '**/*.txt'], // tell vite to allow XML/TXT as static files
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        sitemap: resolve(__dirname, 'public/sitemap.xml'),
-        robots: resolve(__dirname, 'public/robots.txt')
-      }
-    }
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
